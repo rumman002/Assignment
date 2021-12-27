c00316e2e5f51ac2429438da70f93186129a85e1
rumman002 <78809803+rumman002@users.noreply.github.com>
2021-12-27 22:28:23 +0600

    Add files via upload

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Assignment_theme Theme Customizer
+ *
+ * @package Assignment_theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function assignmenttheme_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'assignmenttheme_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'assignmenttheme_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'assignmenttheme_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function assignmenttheme_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function assignmenttheme_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function assignmenttheme_customize_preview_js() {
+	wp_enqueue_script( 'assignmenttheme-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'assignmenttheme_customize_preview_js' );
